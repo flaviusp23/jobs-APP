ca6ac501de5f4ee74918deb9151acd21fddf5d2c
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-05 00:59:44 +0300

    alternative

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -10,7 +10,9 @@ const auth = async (req,res,next) =>{
     const token = authHeader.split(' ')[1]
     try{
         const payload = jwt.verify(token,process.env.JWT_SECRET)
-        req.user = {userId:payload.userId, name:payload.name}
+        // const user = User.findById(payload.id).select('-password')   alternative
+        // req.user = user
+        req.user = {userId:payload.userId, name:payload.name}          //to this
         next()
     }catch (err){
         throw new UnauthenticatedError('Authentication invalid')

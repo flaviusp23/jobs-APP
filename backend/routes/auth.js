f8e7912b3a83fca86497a2ad8933c5439facbec5
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-07 15:21:51 +0300

    time remaining rate limiter api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,11 +7,21 @@ const router = express.Router()
 const rateLimiter = require('express-rate-limit')
 
 const apiLimiter = rateLimiter({
-    windowMs:15*60*1000, // 15minutes
+    windowMs:2*60*1000, // 15minutes
     max:5,
-    message: {
-        msg: 'Too many requests. Please try again later.'
-    }
+    handler: function (req, res, next) {
+        const now = Date.now();
+        const resetTime = req.rateLimit.resetTime || now + this.windowMs;
+        const timeRemaining = Math.max(0, resetTime - now);
+        const minutesRemaining = Math.ceil(timeRemaining / 60000);
+        let msg = `Please try again later in ${minutesRemaining} minutes.`
+        if(minutesRemaining <= 1){
+            msg = `Please try again in less than a minute.`
+        }
+        res.status(429).json({
+          msg: msg,
+        });
+      },
 })
 
 router.post('/login',apiLimiter,login)
